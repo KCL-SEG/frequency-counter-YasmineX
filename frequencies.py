b4f567dc34722c64ed7a97f4be89dd3e42276445
Yasmin Alzaid <1xyasminex@gmail.com>
2023-10-02 14:18:25 +0100

    Completed Exercise

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    stringlst = [str(x) for x in items]
+    for x in stringlst:
+        frequencies[x] = stringlst.count(x)
+
     return frequencies
+
+print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
